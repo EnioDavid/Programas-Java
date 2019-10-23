bd0c2945225fd78204b0a44696e70e98210a8bb3
EnioDavid <44655224+EnioDavid@users.noreply.github.com>
2019-10-23 11:36:55 -0300

    Add files via upload

---
+++ Ado_09.java
@@ -0,0 +1,69 @@
+// Marcelo Frost Marchesan
+// Roger Kevin Pereira Barreto
+// Enio David Santos Mendes Lima
+
+// Atividade ADO 9: Jogo de Adivinhação
+
+package ado_09;
+
+import java.util.Random;
+import java.util.Scanner;
+
+/**
+ *
+ * @author enio.dsmlima
+ */
+public class Ado_09 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Scanner entrada = new Scanner(System.in);
+        Random gerar = new Random();
+        
+        int numero,usuario,result,contador=0,ok = 0;
+        
+        numero = gerar.nextInt(100);
+        
+       
+        
+        do{
+          
+            System.out.printf("%d\n",numero);
+             
+            if(contador == 0){
+                System.out.println("Adivinhe o numero gerado: ");
+                usuario = entrada.nextInt();
+            }
+            else{
+                System.out.println("digite um outro numero: ");
+                usuario = entrada.nextInt();
+            }
+             
+            if(usuario < numero){
+                result = numero-usuario;
+            }
+            else{
+                result = usuario-numero;
+            }
+
+            if(result == 1){
+                System.out.println("TÁ QUENTE");
+            }
+            else if(usuario == numero){
+                System.out.println("Parabéns você acertou");
+                ok = 1;
+            }
+            
+            contador++;
+            
+            if(contador == 5){
+                System.out.println("\n Quantidade de tentativas excedidas.\n");
+            }
+            
+        }while(ok != 1 && contador <=4);
+        
+    }
+    
+}
